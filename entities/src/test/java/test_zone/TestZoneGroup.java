5d203c62457309b141d0341c79d80770e59cee09
thibault <thibault@172.29.5.37>
2019-01-23 15:17:29 +0100

    Tests de ZoneGroup

--- entities/src/test/java/test_zone/TestZoneGroup.java
+++ entities/src/test/java/test_zone/TestZoneGroup.java
@@ -1,29 +1,47 @@
 package test_zone;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 import spell.Card;
+import zone.AutoHideZone;
+import zone.AutoRevealZone;
+import zone.Zone;
 import zone.ZoneGroup;
+import zone.ZonePick;
+import zone.ZoneType;
 
 public class TestZoneGroup
 {
+	@Mock
+	private AutoHideZone deck;
+	@Mock
+	private AutoRevealZone hand;
+	@Mock
+	private AutoRevealZone discard;
+	@Mock
+	private AutoRevealZone burn;
+	@Mock
+	private Zone banish;
+	@Mock
+	private Zone voidZ;
+	
+	@InjectMocks
 	private ZoneGroup zoneGroup;
 	
+	
 	private Card[] cards;
-	private Card card1;
-	private Card card2;
-	private Card card3;
-	private Card card4;
-	private Card card5;
 	
 	private Card[] addedCards;
-	private Card card6;
+	
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -37,10 +55,21 @@ public class TestZoneGroup
 	public void setUp() throws Exception {
 		cards = new Card[]
 				{
-						
+						mock(Card.class),
+						mock(Card.class),
+						mock(Card.class),
+						mock(Card.class),
+						mock(Card.class),
 				};
 		
-		//zoneGroup = new ZoneGroup(addedCards);
+		addedCards = new Card[]
+				{
+						mock(Card.class),
+						mock(Card.class),
+						mock(Card.class),
+				};
+		
+		zoneGroup = new ZoneGroup(cards);
 	}
 
 	@After
@@ -52,12 +81,57 @@ public class TestZoneGroup
 
 	@Test
 	public final void testZoneGroup() {
-		fail("Not yet implemented");
+		verify(deck, times(1)).add(cards, ZonePick.DEFAULT);
+	}
+	
+	@Test
+	public final void testZoneGroupCardsEmpty() {
+		cards = new Card[0];
+		zoneGroup = new ZoneGroup(cards);
+		verify(deck, times(1)).add(cards, ZonePick.DEFAULT);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public final void testZoneGroupException1() {
+		zoneGroup = new ZoneGroup(null);
 	}
 
 	@Test
-	public final void testTransfer() {
-		fail("Not yet implemented");
+	public final void testTransfer()
+	{
+		zoneGroup.transfer(ZoneType.BURN, ZonePick.BOTTOM, ZoneType.BANISH, ZonePick.DEFAULT, 2);
+		Card[] removedCards = new Card[]
+				{
+						mock(Card.class),
+						mock(Card.class),
+				};
+		when(burn.remove(2)).thenReturn(removedCards);
+		verify(burn, times(1)).remove(2, ZonePick.BOTTOM);
+		verify(banish, times(1)).add(removedCards, ZonePick.DEFAULT);
+		
+		
+		zoneGroup.transfer(ZoneType.DECK, ZonePick.TOP, ZoneType.HAND, ZonePick.RANDOM, 3);
+		removedCards = new Card[]
+				{
+						mock(Card.class),
+						mock(Card.class),
+						mock(Card.class),
+				};
+		when(deck.remove(3)).thenReturn(removedCards);
+		verify(deck, times(1)).remove(3, ZonePick.TOP);
+		verify(hand, times(1)).add(removedCards, ZonePick.RANDOM);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public final void testTransferException1()
+	{
+		zoneGroup.transfer(ZoneType.DECK, null, ZoneType.HAND, ZonePick.RANDOM, 3);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public final void testTransferException2()
+	{
+		zoneGroup.transfer(ZoneType.DECK, null, ZoneType.HAND, ZonePick.RANDOM, 3);
 	}
 
 	@Test
@@ -87,12 +161,27 @@ public class TestZoneGroup
 
 	@Test
 	public final void testShuffle() {
-		fail("Not yet implemented");
+		zoneGroup.shuffle(ZoneType.VOID);
+		verify(voidZ, times(1)).shuffle();
+		
+		zoneGroup.shuffle(ZoneType.DISCARD);
+		verify(discard, times(1)).shuffle();
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public final void testShuffleException() {
+		zoneGroup.shuffle(null);
 	}
 
 	@Test
 	public final void testMoveCardToIndex() {
-		fail("Not yet implemented");
+		zoneGroup.moveCardToIndex(ZoneType.HAND, 0, 2);
+		verify(hand, times(1)).moveCardToIndex(0, 2);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public final void testMoveCardToIndexException() {
+		zoneGroup.moveCardToIndex(null, 0, 2);
 	}
 
 }
