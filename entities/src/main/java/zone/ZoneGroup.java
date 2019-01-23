986ceffdbe35fc875f32c4227ba358fc525e3a83
thibault <thibault@172.29.5.37>
2019-01-23 14:16:18 +0100

    ZoneGroup prototypes

--- entities/src/main/java/zone/ZoneGroup.java
+++ entities/src/main/java/zone/ZoneGroup.java
@@ -1,11 +1,23 @@
 package zone;
 
+import spell.Card;
+
 public class ZoneGroup
 {
 	
-	public ZoneGroup()
+	public ZoneGroup(Card[] cards)
 	{
 		
 	}
+	
+	public void transfer(ZoneType source, ZonePick sourcePick, ZoneType dest, ZonePick destPick, int nbCard) {}
+	public void add(Card[] cards, ZoneType zoneType, ZonePick zonePick) {}
+	public void add(Card[] cards, ZoneType zoneType) {}
+	public Card[] remove(int nbCard, ZoneType zoneType, ZonePick zonePick) { return null;}
+	public Card[] remove(int nbCard, ZoneType zoneType) { return null;}
+	public Card[] getCards(ZoneType zoneType) { return null;}
+	public void shuffle(ZoneType zoneType) {}
+	public void moveCardToIndex(ZoneType zoneType, int sourceIndex, int destIndex) {}
+
 	
 }
