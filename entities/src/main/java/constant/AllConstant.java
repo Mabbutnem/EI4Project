1aaf241d3302749ce60aa3954c189f06d02cad4f
thibault <thibault@Thibault>
2019-02-05 18:04:13 +0100

    Constants

--- entities/src/main/java/constant/AllConstant.java
+++ entities/src/main/java/constant/AllConstant.java
@@ -2,12 +2,23 @@ package constant;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
+import boardelement.Wizard;
+
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class AllConstant
 {
+	private WizardConstant wizardConstant;
 
 	public AllConstant() {
 		//Empty constructor for jackson
+	}
+	public AllConstant(WizardConstant wizardConstant){
+		this.wizardConstant = wizardConstant;
+	}
+	
+	public void initAllConstant()
+	{
+		Wizard.setWizardConstant(wizardConstant);
 	}
 	
 }
--- entities/src/main/java/constant/WizardConstant.java
+++ entities/src/main/java/constant/WizardConstant.java
@@ -1,6 +1,7 @@
 package constant;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.google.common.base.Preconditions;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class WizardConstant
@@ -15,6 +16,19 @@ public class WizardConstant
 
 	public WizardConstant() {
 		//Empty constructor for jackson
+	}
+	public WizardConstant(int maxHealth, int initArmor, int baseMove, int baseRange, int baseMana) {
+		Preconditions.checkState(maxHealth > 0, "maxHealth was %s but expected strictly positive", maxHealth);
+		Preconditions.checkState(initArmor >= 0, "initArmor was %s but expected positive", initArmor);
+		Preconditions.checkState(baseMove > 0, "baseMove was %s but expected strictly positive", baseMove);
+		Preconditions.checkState(baseRange > 0, "baseRange was %s but expected strictly positive", baseRange);
+		Preconditions.checkState(baseMana > 0, "baseMana was %s but expected strictly positive", baseMana);
+		
+		this.maxHealth = maxHealth;
+		this.initArmor = initArmor;
+		this.baseMove = baseMove;
+		this.baseRange = baseRange;
+		this.baseMana = baseMana;
 	}
 
 
