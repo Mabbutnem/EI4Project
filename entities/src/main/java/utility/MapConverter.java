26c9f06fed6ce053bbb7c15effdfa2f58340862c
thibault <thibault@Thibault>
2019-02-20 17:52:36 +0100

    MapConverter

--- entities/src/main/java/utility/MapConverter.java
+++ entities/src/main/java/utility/MapConverter.java
@@ -1,6 +1,10 @@
 package utility;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class MapConverter
 {
@@ -14,15 +18,36 @@ public class MapConverter
 	public static INamedObject[] getObjectsFromMapNamesFrequencies(Map<String, Integer> mapNamesFrequencies, INamedObject[] namedObjects)
 	//namedObjects : all namedObjects from the JSON file
 	{
-		//TODO
-		return null;
+		Map<String, INamedObject> namedObjectsMap = new HashMap<>();
+		for(INamedObject no : namedObjects) { namedObjectsMap.put(no.getName(), no); }
+		
+		List<INamedObject> list = new LinkedList<>();
+		
+		//On ajoute les namedObjects dans list
+		for(String namedObjectName : mapNamesFrequencies.keySet())
+		{
+			if(!namedObjectsMap.containsKey(namedObjectName)) { throw new IllegalArgumentException("one (or more) namedObject is missing from namedObjects"); }
+
+			//Ajoute une COPIE du namedObject
+			list.add(namedObjectsMap.get(namedObjectName).cloneObject());
+		}
+		
+		return list.toArray(new INamedObject[0]);
 	}
 	
-	public static int[] getFrequenciesFromMapNamesFrequencies(Map<String, Integer> mapNamesFrequencies, INamedObject[] namedObjects)
-	//namedObjects : all namedObjects from the JSON file
+	public static int[] getFrequenciesFromMapNamesFrequencies(Map<String, Integer> mapNamesFrequencies, INamedObject[] actualNamedObjects)
+	//actualNamedObjects : namedObjects from the map conversion
 	{
-		//TODO
-		return null;
+		int[] frequencies = new int[actualNamedObjects.length];
+		
+		for(int i = 0; i < actualNamedObjects.length; i++)
+		{
+			if(!mapNamesFrequencies.containsKey(actualNamedObjects[i].getName())) { throw new IllegalArgumentException("one (or more) key is missing from mapNamesFrequencies"); }
+			
+			frequencies[i] = mapNamesFrequencies.get(actualNamedObjects[i].getName());
+		}
+		
+		return frequencies;
 	}
 	
 	
@@ -30,7 +55,23 @@ public class MapConverter
 	public static INamedObject[] getObjectsFromMapNamesQuantities(Map<String, Integer> mapNamesQuantities, INamedObject[] namedObjects)
 	//namedObjects : all namedObjects from the JSON file
 	{
-		//TODO
-		return null;
+		Map<String, INamedObject> namedObjectsMap = new HashMap<>();
+		for(INamedObject no : namedObjects) { namedObjectsMap.put(no.getName(), no); }
+		
+		List<INamedObject> list = new LinkedList<>();
+		
+		//On ajoute les namedObjects dans list
+		for(Entry<String, Integer> entry : mapNamesQuantities.entrySet())
+		{
+			if(!namedObjectsMap.containsKey(entry.getKey())) { throw new IllegalArgumentException("one (or more) namedObject is missing from namedObjects"); }
+
+			//Ajoute une COPIE du namedObject
+			for(int i = 0; i < entry.getValue(); i++)
+			{
+				list.add(namedObjectsMap.get(entry.getKey()).cloneObject());
+			}
+		}
+		
+		return list.toArray(new INamedObject[0]);
 	}
 }
